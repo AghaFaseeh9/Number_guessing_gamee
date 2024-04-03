e80e2f987be6c4110eb2fa0e2333d0be3be62134
Agha faseeh <aghafaseeh07@gmail.com>
2024-04-04 00:28:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    message: "Please guess a number: ",
+    name: "userGuessNumber",
+    type: "Number",
+  },
+]);
+
+if (answer.userGuessNumber === randomNumber) {
+  console.log("Congratulation!You guess the right Number");
+} else {
+  console.log("Try Again!You guessed the wrong Number");
+}
